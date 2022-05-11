9b4ce7e8e6419c53263c5f1a92345635fe86f348
Egor <104843220+EgorNovikov76@users.noreply.github.com>
2022-05-11 19:44:41 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,29 +5,29 @@ from art import tprint
 
 
 def pdf_to_mp3(file_path='test.pdf', language='en'):
-    if Path(file_path).is_file() and Path(file_path).suffix == '.pdf':
+    if Path(file_path).is_file() and Path(file_path).suffix == '.pdf':  #проверка расширения файла
 
         print(f'[+] Original file: {Path(file_path).name}')
         print('[+] Processing...')
 
         with pdfplumber.PDF(open(file=file_path, mode='rb')) as pdf:
-            pages = [page.extract_text() for page in pdf.pages]
+            pages = [page.extract_text() for page in pdf.pages]  #извлекаем текст с каждой страницы
 
-        text = ''.join(pages)
-        text = text.replace('\n', '')
+        text = ''.join(pages)                # склеиваем страницы
+        text = text.replace('\n', '')        # удаляе переносы строк
 
         my_audio = gTTS(text=text, lang=language, slow=False)
         file_name = Path(file_path).stem
         my_audio.save(f'{file_name}.mp3')
 
-        return f'[+] {file_name}.mp3 saved successfully!\n---Have a good day!---'
+        return f'[+] {file_name}.mp3 saved successfully!'
 
     else:
         return 'File not exists, check the file path!'
 
 
 def main():
-    tprint('PDF>>TO>>MP3', font='bulbhead')
+    tprint('PDF>>2>>MP3', font='bulbhead')
     file_path = input("\nEnter a file's path: ")
     language = input("Choose language, for example 'en' or 'ru': ")
     print(pdf_to_mp3(file_path=file_path, language=language))
